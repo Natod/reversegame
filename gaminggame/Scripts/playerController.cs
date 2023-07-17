3096d27d627e243453627a6ad59f2c14e17939b1
May <97141450+Natod@users.noreply.github.com>
2023-07-16 17:05:29 -0700

    rotation lerp

--- gaminggame/Scripts/playerController.cs
+++ gaminggame/Scripts/playerController.cs
@@ -8,7 +8,7 @@ public partial class playerController : CharacterBody2D
 	[Export]
 	private int maxHp {get; set;} = 100;
 	[Export]
-	private int moveSpeed {get; set;} = 500;
+	private int moveSpeed {get; set;} = 300;
 	[Export]
 	private int damage {get; set;} = 10;
 	[Export]
@@ -17,50 +17,52 @@ public partial class playerController : CharacterBody2D
 	[Export]
 	private float friction {get; set;} = 0.4f;
 	
-	Vector2 velocity = Input.GetVector("in_left", "in_right", "in_up", "in_down");
+	//Vector2 velocity = Input.GetVector("in_left", "in_right", "in_up", "in_down");
 	
 	public override void _Ready()
 	{
 		moveSpeed *= (int)(1f/(1f-friction));
 	}
 	
+	
+	
 	public override void _PhysicsProcess(double delta)
 	{
-		
-		velocity = Velocity;
+		Vector2 velocity = Velocity;
 		velocity.X *= 1f-friction;
 		velocity.Y *= 1f-friction;
 
 		if (Input.IsActionPressed("in_left") && !Input.IsActionPressed("in_right"))
 		{
 			velocity.X = -moveSpeed;
+			RotationLerp(Mathf.Pi, delta);
 		}
 		else if (Input.IsActionPressed("in_right") && !Input.IsActionPressed("in_left"))
 		{
 			velocity.X = moveSpeed;
-		}
-		else
-		{
-			// velocity.X = 0;
+			RotationLerp(0, delta);
 		}
 		
 		if (Input.IsActionPressed("in_up") && !Input.IsActionPressed("in_down"))
 		{
 			velocity.Y = -moveSpeed;
+			RotationLerp(Mathf.Pi*3/2, delta);
 		}
 		else if (Input.IsActionPressed("in_down") && !Input.IsActionPressed("in_up"))
 		{
 			velocity.Y = moveSpeed;
-		}
-		else
-		{
-			// velocity.Y = 0;
+			RotationLerp(Mathf.Pi/2, delta);
 		}
 		
 
-		Velocity += velocity;
+		Velocity = velocity;
 
 		// "MoveAndSlide" already takes delta time into account.
 		MoveAndSlide();
+	}
+	
+	public void RotationLerp(float newAngle, double delta)
+	{
+		Rotation = Mathf.LerpAngle(Rotation, newAngle, 0.35f);
 	}
 }
